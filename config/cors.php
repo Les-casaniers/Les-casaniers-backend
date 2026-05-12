8d0a38d8c8f881026a608999ab489a1ba90f409d
andritina <tonny.andritina@gmail.com>
2026-05-12 16:00:05 +0300

    CORS

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,8 @@ return [
         env('FRONTEND_URL', 'http://localhost:3000'),
         'http://127.0.0.1:8000',
         'http://172.26.160.1:8080',
+        'http://localhost:8080',
+        'https://api.holines.xyz/',
     ],
 
     'allowed_origins_patterns' => [],
