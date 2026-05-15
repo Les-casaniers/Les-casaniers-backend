4f4648b1bf7727f7d58ab126aed023233300a33e
ARIA SERVICE TECH <dev.ariamada@gmail.com>
2026-05-15 10:36:31 +0300

    Create AdminSeeder.php

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('admin')->insertOrIgnore([
+            'prenom'            => 'Admin',
+            'nom'               => 'Les casaniers',
+            'email'             => 'contact@lc-cie.com',
+            'telephone'         => '0385157042',
+            'mot_de_passe'      => Hash::make('AdminCasaniers2026-'),
+            'poste'             => 'admin',
+            'statut'            => 'actif',
+            'date_creation'     => now(),
+            'date_modification' => now(),
+        ]);
+    }
+}
